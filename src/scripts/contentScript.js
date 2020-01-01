a2255dc029b86c8cf365ff0d3824ba90d7abc9ca
lowwebtech <vico@lowweb.tech>
2020-01-01 12:30:20 +0100

    replace Jeff Bezos by Jeff Devil Bezos

--- src/scripts/contentScript.js
+++ src/scripts/contentScript.js
@@ -1,1 +1,18 @@
-document.body.style.filter = 'invert(1)';
+const elements = document.getElementsByTagName('*');
+
+for (let i = 0; i < elements.length; i++) {
+  let element = elements[i];
+
+  for (let j = 0; j < element.childNodes.length; j++) {
+    let node = element.childNodes[j];
+
+    if (node.nodeType === 3) {
+      let text = node.nodeValue;
+      let replacedText = text.replace(/Jeff Bezos/gi, 'Jeff Devil Bezos');
+
+      if (replacedText !== text) {
+        element.replaceChild(document.createTextNode(replacedText), node);
+      }
+    }
+  }
+}
