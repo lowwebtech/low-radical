14907a12fcdd276172a0f2ec9a9e23b9f3cacc6b
Vico <vico@lowweb.tech>
2020-06-23 02:21:51 +0200

    performance Jeff Bezos replacement

--- src/scripts/contentScript.js
+++ src/scripts/contentScript.js
@@ -7,15 +7,17 @@ function onGot(result){
   const elements = document.getElementsByTagName('*');
   const replaceBy = result.replaceBy || 'Jeff 🖕 Bezos';
 
-  for (let i = 0; i < elements.length; i++) {
+  for (let i = 0, lg = elements.length; i < lg; i++) {
     let element = elements[i];
-    for (let j = 0; j < element.childNodes.length; j++) {
+    for (let j = 0, lgj = element.childNodes.length; j < lgj; j++) {
       let node = element.childNodes[j];
       if (node.nodeType === 3) {
         let text = node.nodeValue;
-        let replacedText = text.replace(/Jeff Bezos/gi, replaceBy).replace(/Jeffrey Bezos/gi, replaceBy);
-        if (replacedText !== text) {
-          element.replaceChild(document.createTextNode(replacedText), node);
+        if(text.indexOf('Bezos') !== -1){
+          let replacedText = text.replace(/Jeff Bezos/gi, replaceBy).replace(/Jeffrey Bezos/gi, replaceBy).replace(/Jeffrey Preston Bezos/gi, replaceBy);
+          if (replacedText !== text) {
+            element.replaceChild(document.createTextNode(replacedText), node);
+          } 
         }
       }
     }
