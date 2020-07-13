efbd6db6b318688ef5579047cdae29dcb3becfcc
Vico <vico@lowweb.tech>
2020-07-14 01:26:53 +0200

    optional AWS detect & replace Jeff Bezos name

--- src/scripts/comp/form.js
+++ src/scripts/comp/form.js
@@ -1,21 +1,64 @@
 import browser from 'webextension-polyfill';
 
+const Optional = class{
+  constructor( el ){
+    this.el = el
+    this.id = el.id
 
-const replaceByEl = document.querySelector('input[name="replaceBy"]')
-const awsDetectEl = document.querySelector('input[name="awsDetect"]')
+    this.active = false
+    this.value = ''
+
+    this.cbEl = this.el.querySelector('.optional__cb')
+    this.valueEl = this.el.querySelector('.optional__value')
+
+    this.cbEl.addEventListener('input', ()=>{
+      this.update()
+    })
+  }
+
+  update(){
+    this.active = this.cbEl.checked
+    if(this.active){
+      this.el.classList.add('optional--active')
+    }else{
+      this.el.classList.remove('optional--active')
+    }
+  }
+
+  restore(data){
+    
+    if(data){
+      this.cbEl.checked = data.active
+      this.valueEl.value = data.value
+    }else{
+      this.active = this.cbEl.checked  
+      this.value = this.valueEl.value
+    }
+    
+    this.update()
+  }
+
+  getData(){
+    this.active = this.cbEl.checked
+    this.value = this.valueEl.value
+
+    return {
+      active: this.active,
+      value: this.value,
+    }
+  }
+}
 
 function save() {
-  const data = {
+  let data = {
     blockType: document.querySelector('input[name="blockType"]:checked').value,
   }
 
-  if(replaceByEl){
-    data.replaceBy = replaceByEl.value
+  for(let i = 0, lg = optionals.length; i<lg; i++){
+    let optional = optionals[i];
+    data[ optional.id ] = optional.getData()
   }
-  if(awsDetectEl){
-    data.awsDetect = awsDetectEl.value
-  }
-  
+
   browser.storage.local.set( data );
 }
 
@@ -23,18 +66,12 @@ function restore() {
   let getting = browser.storage.local.get(['blockType', 'replaceBy', 'awsDetect']);
   getting.then(
     (result) => {
-      let blockType = result.blockType || 'blockAll'
+      const blockType = result.blockType || 'blockAll'
       document.querySelector('input[value="'+ blockType + '"]').checked = true
 
-      if(replaceByEl){
-        let replaceBy = result.replaceBy || 'Jeff 🖕 Bezos'
-        replaceByEl.value = replaceBy 
-      }
-
-      if(awsDetectEl){
-        console.log('AWS', result.awsDetect, awsDetect) 
-        let awsDetect = result.awsDetect
-        awsDetectEl.checked = awsDetect
+      for(let i = 0, lg = optionals.length; i<lg; i++){
+        let optional = optionals[i];
+        optional.restore( result[ optional.id ] )
       }
     }, 
     (error) => {
@@ -42,12 +79,21 @@ function restore() {
     });
 }
 
+
+const optionals = []
+
 document.addEventListener('DOMContentLoaded', ()=>{
+  const optionalEls = document.querySelectorAll('.optional')
+  optionalEls.forEach((el)=>{
+    optionals.push( new Optional(el) )
+  })
+
   let inputs = document.querySelectorAll('input')
   inputs.forEach((input)=>{
     input.addEventListener('input', (e)=>{
       save()
     })
   })
+
   restore()
 });
