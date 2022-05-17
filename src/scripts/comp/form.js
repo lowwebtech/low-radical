d13d723e991b45ae42d446d9f0fbef7ecce6b332
lowwebtech <vico@lowweb.tech>
2022-05-17 23:18:22 +0200

    small change to popup/options pages

--- src/scripts/comp/form.js
+++ src/scripts/comp/form.js
@@ -1,28 +1,28 @@
-import { getLocalParams, setParams } from "../data/params";
-import { all } from "../data/datas";
+import { getLocalParams, setParams } from '../data/params'
+import { all } from '../data/datas'
 
 function save() {
-  const data = {};
+  const data = {}
 
-  const inputs = document.querySelectorAll("input[type=checkbox]");
+  const inputs = document.querySelectorAll('input[type=checkbox]')
   inputs.forEach((input) => {
-    data[input.name] = input.checked;
-  });
+    data[input.name] = input.checked
+  })
 
-  setParams(data);
+  setParams(data)
 }
 
 async function restore() {
-  const getting = getLocalParams();
+  const getting = getLocalParams()
 
   await getting.then(
     (local) => {
-      const fragment = new DocumentFragment();
+      const fragment = new DocumentFragment()
       all.forEach((group) => {
-        const checked = local[group.id] ? "checked" : "";
-        const li = document.createElement("li");
-        li.classList.add("group");
-        if (checked) li.classList.add("active");
+        const checked = local[group.id] ? 'checked' : ''
+        const li = document.createElement('li')
+        li.classList.add('group')
+        if (checked) li.classList.add('active')
 
         li.innerHTML = `
         <p class="option">
@@ -39,15 +39,15 @@ async function restore() {
             />
           </span>
         </p>
-        `;
+        `
 
-        const sub = document.createElement("div");
-        sub.classList.add("subgroup");
-        const sublist = document.createElement("ul");
+        const sub = document.createElement('div')
+        sub.classList.add('subgroup')
+        const sublist = document.createElement('ul')
         group.subgroups.forEach((subgroup) => {
-          const checked = local[subgroup.id] ? "checked" : "";
+          const checked = local[subgroup.id] ? 'checked' : ''
 
-          const subli = document.createElement("li");
+          const subli = document.createElement('li')
           subli.innerHTML = `
           <p class="option">
             <label for="${subgroup.id}">${subgroup.name}</label>
@@ -60,59 +60,61 @@ async function restore() {
               ${checked}
             />
           </p>
-          `;
-          sublist.appendChild(subli);
-        });
-        sub.appendChild(sublist);
+          `
+          sublist.appendChild(subli)
+        })
+        sub.appendChild(sublist)
 
-        li.appendChild(sub);
-        fragment.appendChild(li);
-      });
+        li.appendChild(sub)
+        fragment.appendChild(li)
+      })
 
-      document.querySelector("form > ul").appendChild(fragment);
+      const list = document.createElement('ul')
+      list.appendChild(fragment)
+      document.querySelector('form').appendChild(list)
     },
     (error) => {
-      console.log(`Error: ${error}`);
-    }
-  );
+      console.log(`Error: ${error}`)
+    },
+  )
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   restore().then(() => {
-    const inputs = document.querySelectorAll("input");
+    const inputs = document.querySelectorAll('input')
     inputs.forEach((input) => {
-      input.addEventListener("input", (e) => {
-        save();
-      });
-    });
+      input.addEventListener('input', (e) => {
+        save()
+      })
+    })
 
-    const groups = document.querySelectorAll(".group");
+    const groups = document.querySelectorAll('.group')
     groups.forEach((group) => {
-      const inputsGroup = group.querySelectorAll(":scope > .option input");
+      const inputsGroup = group.querySelectorAll(':scope > .option input')
       inputsGroup.forEach((input) => {
-        console.log(input);
-        input.addEventListener("input", (e) => {
+        console.log(input)
+        input.addEventListener('input', (e) => {
           if (e.currentTarget.checked) {
-            group.classList.add("active");
+            group.classList.add('active')
           } else {
-            group.classList.remove("active");
+            group.classList.remove('active')
           }
-        });
-      });
+        })
+      })
 
-      const buttonGroup = group.querySelectorAll(":scope > .option button");
+      const buttonGroup = group.querySelectorAll(':scope > .option button')
       buttonGroup.forEach((button) => {
-        button.addEventListener("click", (e) => {
-          e.preventDefault();
-          if (group.classList.contains("opened")) {
-            group.classList.remove("opened");
-            button.innerHTML = "Whitelist";
+        button.addEventListener('click', (e) => {
+          e.preventDefault()
+          if (group.classList.contains('opened')) {
+            group.classList.remove('opened')
+            button.innerHTML = 'Whitelist'
           } else {
-            group.classList.add("opened");
-            button.innerHTML = "Close";
+            group.classList.add('opened')
+            button.innerHTML = 'Close'
           }
-        });
-      });
-    });
-  }, console.error);
-});
+        })
+      })
+    })
+  }, console.error)
+})
